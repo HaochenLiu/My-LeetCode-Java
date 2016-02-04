23c7678361631f5179231bdcec724008267404bb
Haochen Liu <liuhaochen1221@hotmail.com>
2016-02-04 10:20:27 -0800

    Update ReverseInteger.java

--- ReverseInteger.java
+++ ReverseInteger.java
@@ -18,3 +18,17 @@ For the purpose of this problem, assume that your function returns 0 when the re
 Update (2014-11-10):
 Test cases had been added to test the overflow behavior.
 */
+
+public class Solution {
+    public int reverse(int x) {
+        int rev = 0;
+        while (x != 0) {
+            //Handel the buffer overflow : should be handeled before rev update 
+            if (rev != 0 && Integer.MAX_VALUE / rev < 10 && Integer.MAX_VALUE / rev > -10)
+                return 0;
+            rev = rev * 10 + x % 10;
+            x = x / 10;
+        }
+        return rev ;
+    }
+}
