139f8bb690b8edc349fb8ef3057083715c62423c
Haochen Liu <liuhaochen1221@hotmail.com>
2016-02-07 13:39:49 -0800

    Create BinaryTreeZigzagLevelOrderTraversal.java

---
+++ BinaryTreeZigzagLevelOrderTraversal.java
@@ -0,0 +1,58 @@
+/*
+Given a binary tree, return the zigzag level order traversal of its nodes' values. (ie, from left to right, then right to left for the next level and alternate between).
+
+For example:
+Given binary tree {3,9,20,#,#,15,7},
+    3
+   / \
+  9  20
+    /  \
+   15   7
+return its zigzag level order traversal as:
+[
+  [3],
+  [20,9],
+  [15,7]
+]
+confused what "{1,#,2,3}" means? > read more on how binary tree is serialized on OJ.
+*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        if (root == null)
+            return result;
+        List<Integer> list = new ArrayList<Integer>();
+        LinkedList<TreeNode> curLevel = new LinkedList<TreeNode>();
+        LinkedList<TreeNode> nextLevel = new LinkedList<TreeNode>();
+        curLevel.add(root);
+        boolean isL2R = true;
+        while (!curLevel.isEmpty()) {
+            TreeNode cur = curLevel.remove();
+            list.add(cur.val);
+            if (cur.left != null)
+                nextLevel.add(cur.left);
+            if (cur.right != null)
+                nextLevel.add(cur.right);
+            if (curLevel.isEmpty()) {
+                if (!isL2R)
+                    Collections.reverse(list);
+                isL2R = !isL2R;
+                result.add(list);
+                curLevel = nextLevel;
+                nextLevel = new LinkedList<TreeNode>();
+                list = new ArrayList<Integer>();
+            }
+        }
+        return result;
+    }
+}
