1b242cce929e72b1986dd1c009d9d951b0adf0aa
Haochen Liu <liuhaochen1221@hotmail.com>
2016-02-07 15:46:21 -0800

    Create BinaryTreeRightSideView.java

---
+++ BinaryTreeRightSideView.java
@@ -0,0 +1,48 @@
+/*
+Given a binary tree, imagine yourself standing on the right side of it, return the values of the nodes you can see ordered from top to bottom.
+
+For example:
+Given the following binary tree,
+   1            <---
+ /   \
+2     3         <---
+ \     \
+  5     4       <---
+You should return [1, 3, 4].
+
+Credits:
+Special thanks to @amrsaqr for adding this problem and creating all test cases.
+*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> result = new ArrayList<Integer>();
+        if (root == null)
+            return result;
+        LinkedList<TreeNode> list = new LinkedList<TreeNode>();
+        LinkedList<TreeNode> nextLevel = new LinkedList<TreeNode>();
+        list.add(root);
+        while (!list.isEmpty()) {
+            TreeNode node = list.remove();
+            if (node.left != null)
+                nextLevel.add(node.left);
+            if (node.right != null)
+                nextLevel.add(node.right);
+            if (list.isEmpty()) {
+                result.add(node.val);
+                list = nextLevel;
+                nextLevel = new LinkedList<TreeNode>();
+            }
+        }
+        return result;
+    }
+}
