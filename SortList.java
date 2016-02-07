47da6b9b989a72216a1c1cda13f7f2234a07783f
Haochen Liu <liuhaochen1221@hotmail.com>
2016-02-07 14:41:26 -0800

    Create SortList.java

---
+++ SortList.java
@@ -0,0 +1,33 @@
+/*
+Sort a linked list in O(n log n) time using constant space complexity.
+*/
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null)
+            return head;
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        ListNode cur = head;
+        while (cur != null) {
+            list.add(cur.val);
+            cur = cur.next;
+        }
+        Collections.sort(list);
+        cur = head;
+        int i = 0;
+        while (cur != null) {
+            cur.val = list.get(i);
+            cur = cur.next;
+            i++;
+        }
+        return head;
+    }
+}
