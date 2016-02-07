38d16ba2be4221ea38de62713b181c9e7476dc63
Haochen Liu <liuhaochen1221@hotmail.com>
2016-02-06 22:05:33 -0800

    Create Permutations.java

---
+++ Permutations.java
@@ -0,0 +1,26 @@
+/*
+Given a collection of distinct numbers, return all possible permutations.
+
+For example,
+[1,2,3] have the following permutations:
+[1,2,3], [1,3,2], [2,1,3], [2,3,1], [3,1,2], and [3,2,1].
+*/
+
+public class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        result.add(new ArrayList<Integer>());
+        for (int i = 0; i < nums.length; i++) {
+            List<List<Integer>> tmp = new ArrayList<List<Integer>>();
+            for (List<Integer> list : result) {
+                for (int j = 0; j <= list.size(); j++) {
+                    List<Integer> item = new ArrayList<Integer>(list);
+                    item.add(j, nums[i]);
+                    tmp.add(item);
+                }
+            }
+            result = tmp;
+        }
+        return result;
+    }
+}
