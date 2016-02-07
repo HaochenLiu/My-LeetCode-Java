7f184567c79ac211c7fec16717540dea397529b9
Haochen Liu <liuhaochen1221@hotmail.com>
2016-02-06 22:57:06 -0800

    Create RemoveDuplicatesFromSortedArrayII.java

---
+++ RemoveDuplicatesFromSortedArrayII.java
@@ -0,0 +1,24 @@
+/*
+Follow up for "Remove Duplicates":
+What if duplicates are allowed at most twice?
+
+For example,
+Given sorted array nums = [1,1,1,2,2,3],
+
+Your function should return length = 5, with the first five elements of nums being 1, 1, 2, 2 and 3. It doesn't matter what you leave beyond the new length.
+*/
+
+public class Solution {
+    public int removeDuplicates(int[] nums) {
+        if (nums.length == 0)
+            return nums.length;
+        int count = 1;
+        int index = 1;
+        for (int i = 1; i < nums.length; i++) {
+            count = nums[i] == nums[i - 1] ? count + 1 : 1;
+            if (count <= 2)
+                nums[index++] = nums[i];
+        }
+        return index;
+    }
+}
