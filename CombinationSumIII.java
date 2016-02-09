5ae39f79e4193491973964014abb9a42d7119da3
Haochen Liu <liuhaochen1221@hotmail.com>
2016-02-09 11:42:28 -0800

    Update CombinationSumIII.java

--- CombinationSumIII.java
+++ CombinationSumIII.java
@@ -21,3 +21,25 @@ Output:
 [[1,2,6], [1,3,5], [2,3,4]]
 Credits:
 Special thanks to @mithmatt for adding this problem and creating all test cases.
+*/
+
+public class Solution {
+    public List<List<Integer>> combinationSum3(int k, int n) {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        dfs(result, new ArrayList<Integer>(), 1, k, n);
+        return result;
+    }
+
+    public void dfs(List<List<Integer>> result, List<Integer> list, 
+            int position, int k, int n) {
+        if (k == 0 && n == 0) {
+            result.add(new ArrayList<Integer>(list));
+            return;
+        }
+        for (int i = position; i <= 9 && n > 0; ++i) {
+            list.add(i);
+            dfs(result, list, i + 1, k - 1, n - i);
+            list.remove(list.size() - 1);
+        }
+    }
+}
