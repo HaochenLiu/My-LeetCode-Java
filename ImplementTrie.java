bf5b71274137235319276b4cdb15e19f3c32b19f
Haochen Liu <liuhaochen1221@hotmail.com>
2016-02-09 11:31:24 -0800

    Create ImplementTrie.java

---
+++ ImplementTrie.java
@@ -0,0 +1,60 @@
+/*
+Implement a trie with insert, search, and startsWith methods.
+
+Note:
+You may assume that all inputs are consist of lowercase letters a-z.
+*/
+
+class TrieNode {
+    // Initialize your data structure here.
+    public TrieNode[] children;
+    public String item;
+
+    public TrieNode() {
+        children = new TrieNode[26];
+        item = "";
+    }
+}
+
+public class Trie {
+    private TrieNode root;
+
+    public Trie() {
+        root = new TrieNode();
+    }
+
+    public void insert(String word) {
+        TrieNode cur = root;
+        for (char c : word.toCharArray()) {
+            if (cur.children[c - 'a'] == null)
+                cur.children[c - 'a'] = new TrieNode();
+            cur = cur.children[c - 'a'];
+        }
+        cur.item = word;
+    }
+
+    public boolean search(String word) {
+        TrieNode cur = root;
+        for (char c : word.toCharArray()) {
+            if (cur.children[c - 'a'] != null)
+                cur = cur.children[c - 'a'];
+        }
+        return cur.item.equals(word);
+
+    }
+
+    public boolean startsWith(String prefix) {
+        TrieNode cur = root;
+        for (char c : prefix.toCharArray()) {
+            if (cur.children[c - 'a'] == null)
+                return false;
+            cur = cur.children[c - 'a'];
+        }
+        return true;
+    }
+}
+
+// Your Trie object will be instantiated and called as such:
+// Trie trie = new Trie();
+// trie.insert("somestring");
+// trie.search("key");
