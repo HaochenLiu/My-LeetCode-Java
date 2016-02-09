b3c06e0dd509eeac4f8027d4b6c48bb1b71e9d64
Haochen Liu <liuhaochen1221@hotmail.com>
2016-02-09 11:28:02 -0800

    Create ReverseLinkedList.java

---
+++ ReverseLinkedList.java
@@ -0,0 +1,32 @@
+/*
+Reverse a singly linked list.
+
+click to show more hints.
+
+Hint:
+A linked list can be reversed either iteratively or recursively. Could you implement both?
+*/
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode reverseList(ListNode head) {
+        if (head == null)
+            return head;
+        ListNode pre = head;
+        ListNode cur = head.next;
+        while (cur != null) {
+            pre.next = cur.next;
+            cur.next = head;
+            head = cur;
+            cur = pre.next;
+        }
+        return head;
+    }
+}
