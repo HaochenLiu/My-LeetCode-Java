f568afaf25344995f3fb0638e9126c6350a4890c
Haochen Liu <liuhaochen1221@hotmail.com>
2016-03-30 11:26:39 -0700

    Update TwoSum.java

--- TwoSum.java
+++ TwoSum.java
@@ -1,9 +1,15 @@
 /*
-Given an array of integers, find two numbers such that they add up to a specific target number.
-
-The function twoSum should return indices of the two numbers such that they add up to the target, where index1 must be less than index2. Please note that your returned answers (both index1 and index2) are not zero-based.
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
 
 You may assume that each input would have exactly one solution.
+
+Example:
+Given nums = [2, 7, 11, 15], target = 9,
+
+Because nums[0] + nums[1] = 2 + 7 = 9,
+return [0, 1].
+UPDATE (2016/2/13):
+The return format had been changed to zero-based indices. Please read the above updated description carefully.
 */
 
 public class Solution {
@@ -12,7 +18,7 @@ public class Solution {
         int n = nums.length;
         for(int i = 0; i < n; i++) {
             if(m.get(target - nums[i]) != null) {
-                return new int[] {m.get(target - nums[i]) + 1, i + 1};
+                return new int[] {m.get(target - nums[i]), i};
             } else {
                 m.put(nums[i], i);
             }
