00d3c89735f0b489130f4b2939c815c64e5beb22
Haochen Liu <liuhaochen1221@hotmail.com>
2016-09-24 22:55:20 -0700

    Create FindTheDifference.java

---
+++ FindTheDifference.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public char findTheDifference(String s, String t) {
+        char[] sArray = s.toCharArray();
+        char[] tArray = t.toCharArray();
+        char t1 = 0;
+        for(char c1:sArray)
+            t1^=c1;
+        for(char c2:tArray)
+            t1^=c2;
+        return(char)t1;
+    }
+}
