0937d2259bd0fb28b5aade25fd87b0368b4c6e52
Haochen Liu <liuhaochen1221@hotmail.com>
2016-11-26 01:54:12 -0800

    Create MinimumMovesToEqualArrayElements.java

---
+++ MinimumMovesToEqualArrayElements.java
@@ -0,0 +1,16 @@
+public class Solution {
+    public int minMoves(int[] nums) {
+        int min = Integer.MAX_VALUE;
+        
+        for (int i : nums) {
+            min = Math.min(min, i);
+        }
+        
+        int res = 0;
+        for (int i : nums) {
+            res += i - min;
+        }
+        
+        return res;
+    }
+}
